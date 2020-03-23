636d84da5aaede8b149bd485f0f73f31c6a3b44b
Victor Ribeiro <55464528+vsribeiro19@users.noreply.github.com>
2020-03-22 22:37:18 -0300

    Tabuada utilizando o laço de repetição FOR

---
+++ tabuada.php
@@ -0,0 +1,7 @@
+<?php
+    $vInformado = 5;
+    $total;
+    for($i = 0; $i <=10; $i++){
+        $total = $vInformado * $i;
+        echo $vInformado . ' x ' . $i . ' = ' . $total . '<br>'; 
+    }
